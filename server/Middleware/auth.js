a624eecf9031a1dc563169643e81f7ad79e82a11
WEOWM <basims0077@gmail.com>
2024-12-19 12:50:04 +0530

    first

--- server/Middleware/auth.js
+++ server/Middleware/auth.js
@@ -18,7 +18,7 @@ const verifyToken = (req, res, next) => {
         next();
 
     } catch (error) {
-        res.status(400).json({message : 'Invalid token'});
+        res.status(400).json({message :   'Invalid token'});
     }
 
 }
